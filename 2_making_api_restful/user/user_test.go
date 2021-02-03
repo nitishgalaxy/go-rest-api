13922432feea11d6a0ea7e8f86934cf2b887d57a
Nitish Saha <nitisaha@cisco.com>
2021-02-04 02:49:16 +0530

    Added Benchmarks

--- 2_making_api_restful/user/user_test.go
+++ 2_making_api_restful/user/user_test.go
@@ -3,6 +3,7 @@ package user
 import (
 	"os"
 	"reflect"
+	"strconv"
 	"testing"
 
 	"github.com/asdine/storm"
@@ -115,4 +116,196 @@ func TestCrud(t *testing.T) {
 	if len(users) != 3 {
 		t.Errorf("Different no. of records retrieved. Expected 3 / Actual %d", len(users))
 	}
+}
+
+func BenchmarkCrud(b *testing.B) {
+	// Delete DB file
+	os.Remove(dbpath)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		// Log the name of the test
+		// t.Log function ensures only failing tests display this info.
+
+		// Create
+
+		b.Log("Create")
+		u := &User{
+			ID:   bson.NewObjectId(),
+			Name: "John",
+			Role: "Tester",
+		}
+
+		err := u.Save()
+		if err != nil {
+			// t.Fatalf/ t.Fatal is shortcut for t.FatalNow
+			b.Fatalf("Error saving a record: %s ", err)
+		}
+
+		//=========================================
+		// Read
+
+		_, err = One(u.ID)
+		if err != nil {
+			// t.Fatalf/ t.Fatal is shortcut for t.FatalNow
+			b.Fatalf("Error receiving a record: %s ", err)
+		}
+
+		//=========================================
+		// Update
+
+		u.Role = "developer"
+		err = u.Save()
+		if err != nil {
+			// t.Fatalf/ t.Fatal is shortcut for t.FatalNow
+			b.Fatalf("Error updating a record: %s ", err)
+		}
+
+		//=========================================
+		//Delete
+		err = Delete(u.ID)
+		if err != nil {
+			// t.Fatalf/ t.Fatal is shortcut for t.FatalNow
+			b.Fatalf("Error deleting a record: %s ", err)
+		}
+
+		//=========================================
+
+		_, err = All()
+
+		if err != nil {
+			// t.Fatalf/ t.Fatal is shortcut for t.FatalNow
+			b.Fatalf("Error getting all records: %s ", err)
+		}
+	}
+
+}
+
+func cleanDB(b *testing.B) {
+	os.Remove(dbpath)
+	u := &User{
+		ID:   bson.NewObjectId(),
+		Name: "John",
+		Role: "Tester",
+	}
+
+	err := u.Save()
+	if err != nil {
+		// t.Fatalf/ t.Fatal is shortcut for t.FatalNow
+		b.Fatalf("Error saving a record: %s ", err)
+	}
+
+	b.ResetTimer()
+
+}
+
+func BenchmarkCreate(b *testing.B) {
+	cleanDB(b)
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+
+		u := &User{
+			ID:   bson.NewObjectId(),
+			Name: "John_" + strconv.Itoa(i),
+			Role: "Tester",
+		}
+
+		b.StartTimer()
+
+		err := u.Save()
+		if err != nil {
+			// t.Fatalf/ t.Fatal is shortcut for t.FatalNow
+			b.Fatalf("Error saving a record: %s ", err)
+		}
+
+	}
+
+}
+
+func BenchmarkRead(b *testing.B) {
+	cleanDB(b)
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+
+		u := &User{
+			ID:   bson.NewObjectId(),
+			Name: "John_" + strconv.Itoa(i),
+			Role: "Tester",
+		}
+
+		err := u.Save()
+		if err != nil {
+			// t.Fatalf/ t.Fatal is shortcut for t.FatalNow
+			b.Fatalf("Error saving a record: %s ", err)
+		}
+
+		b.StartTimer()
+
+		_, err = One(u.ID)
+		if err != nil {
+			// t.Fatalf/ t.Fatal is shortcut for t.FatalNow
+			b.Fatalf("Error receiving a record: %s ", err)
+		}
+
+	}
+
+}
+
+func BenchmarkUpdate(b *testing.B) {
+	cleanDB(b)
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+
+		u := &User{
+			ID:   bson.NewObjectId(),
+			Name: "John_" + strconv.Itoa(i),
+			Role: "Tester",
+		}
+
+		err := u.Save()
+		if err != nil {
+			// t.Fatalf/ t.Fatal is shortcut for t.FatalNow
+			b.Fatalf("Error saving a record: %s ", err)
+		}
+
+		b.StartTimer()
+
+		u.Role = "developer"
+		err = u.Save()
+		if err != nil {
+			// t.Fatalf/ t.Fatal is shortcut for t.FatalNow
+			b.Fatalf("Error updating a record: %s ", err)
+		}
+
+	}
+
+}
+
+func BenchmarkDelete(b *testing.B) {
+	cleanDB(b)
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+
+		u := &User{
+			ID:   bson.NewObjectId(),
+			Name: "John_" + strconv.Itoa(i),
+			Role: "Tester",
+		}
+
+		err := u.Save()
+		if err != nil {
+			// t.Fatalf/ t.Fatal is shortcut for t.FatalNow
+			b.Fatalf("Error saving a record: %s ", err)
+		}
+
+		b.StartTimer()
+
+		err = Delete(u.ID)
+		if err != nil {
+			// t.Fatalf/ t.Fatal is shortcut for t.FatalNow
+			b.Fatalf("Error deleting a record: %s ", err)
+		}
+
+	}
+
 }
